a24d3b32932d093f65af35de0ece4fdffa6be7ab
Modupi <modupimaphoto@gmail.com>
2024-02-02 21:49:29 +0200

    Importing the whole package

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 """Defines BaseModel module"""
 
 
@@ -14,13 +15,15 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                        d_format = '%Y-%m-%dT%H:%M:%S.%f'
+                        setattr(self, key, datetime.strptime(value, d_format))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -31,6 +34,7 @@ class BaseModel():
         with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
