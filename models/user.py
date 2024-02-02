1a1d8efd04ad5cb08d8277da6e82a522e044c76d
Modupi <modupimaphoto@gmail.com>
2024-02-02 22:05:17 +0200

    8. First User

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Defines a User Class"""
+
+
+class User(BaseModel):
+    """Defines User Class Body"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
