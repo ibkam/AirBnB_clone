7f54ed0f4e7a743e5e8ad1ba5f9b957e1d0a964e
Modupi <modupimaphoto@gmail.com>
2024-02-02 06:30:41 +0200

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,18 @@ class BaseModel():
     Defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
