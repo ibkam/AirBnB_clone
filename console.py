50c71917c76834f00b06eb7b8fa55b37c984a748
Modupi <modupimaphoto@gmail.com>
2024-02-03 12:57:27 +0200

    added all and update command logic

--- console.py
+++ console.py
@@ -2,6 +2,25 @@
 """Defines the HBNBCommand class"""
 
 import cmd
+from shlex import split
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'Amenity': Amenity,
+        'Place': Place,
+        'City': City,
+        'Review': Review
+        }
 
 
 class HBNBCommand(cmd.Cmd):
@@ -45,7 +64,7 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             new_instance = "{}.{}".format(command[0], command[1])
-            objs = models.storage.all()
+            objs = storage.all()
 
             if new_instance not in objs:
                 print("** no instance found **")
@@ -72,15 +91,57 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def do_all(self):
-        """Prints all string representation of all instances
-        based or not on the class name"""
-        pass
+        """All comman: print all instances based one"""
+        instance_list = []
+
+        if not line:
+            for new_instance in storage.all().values():
+                instance_list.append(str(new_instance))
+        else:
+            command = split(line)
+            if command[0] in classes:
+                for key, value in storage.all().items():
+                    if value.__class__.__name__ == command[0]:
+                        instance_list.append(str(value))
+                    else:
+                        print("** class doesn't exist **")
+                        return False
+        print(instance_list)
 
     def do_update(self):
-        """Updates an instance based on the class name and
-        id by adding or updating attribute
-        (save the change into the JSON file)"""
-        pass
+        """Update command: an instance based on the class name and id"""
+        command = split(line)
+        
+        if not command:
+            print("** class name missing **")
+        elif command[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(command[0]) < 2:
+            print("** instance id missing **")
+        elif len(command[0]) < 3:
+            print("** attribute name missing **")
+        elif len(command[0]) < 4:
+            print("** value missing **")
+        else:
+            new_instance = command[0] + '.' + command[1]
+            if new_instance not in storage.all()[new_instance]:
+                print("** no instance found **")
+            else:
+                setattr(storage.all()[new_instance], command[2], command[3])
+                storage.save()
+
+    def default(self, line):
+        i = 0
+        command = line.split('.', 1)
+        if len(command) >= 2:
+            line = command[1].split('c')
+            if line[0] == 'all':
+                self.do_all(command[0])
+            elif line[0] == i:
+                for key, in storage.all():
+                    if command[0] == key.split(".")[0]:
+                        i += 1
+                print(i)
 
 
 if __name__ == '__main__':
