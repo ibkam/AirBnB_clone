e8978bbaef9037f55d587e1d7c801eed3200abf6
ibkam <110990848+ibkam@users.noreply.github.com>
2024-02-19 16:54:50 +0300

    Update test_amenity_model.py

--- tests/test_models/test_amenity_model.py
+++ tests/test_models/test_amenity_model.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""Defines Test Cases Amenity class"""
-
-
-import unittest
-from models.base_model import BaseModel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class TestAmenityModel(unittest.TestCase):
+class test_Amenity(test_basemodel):
+    """ """
 
-    def test_Amenity_inheritence(self):
-        """Checks if Amenity Class inherits BaseModel"""
-        new_amenity = Amenity()
-        self.assertIsInstance(new_amenity, BaseModel)
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
 
-    def test_Amenity_attributes(self):
-        """Checks inf Amenity attributes exist"""
-        new_amenity = Amenity()
-        self.assertTrue("name" in new_amenity.__dir__())
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
