fbc5e59c013acc158af117e390f599a1593cb6f6
ibkam <110990848+ibkam@users.noreply.github.com>
2024-02-20 08:27:34 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-__init__ dunder method for the models directory
-"""
+"""This module instantiates an object of class FileStorage"""
+
 from models.engine.file_storage import FileStorage
 
 
